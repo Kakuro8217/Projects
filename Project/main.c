85a0fb6ba68a2ab6e691fe5bc281067dc9f68162
Kakuro8217 <13766832549@163.com>
2025-09-28 14:06:36 +0800

    first commit

---
+++ Project/main.c
@@ -0,0 +1,81 @@
+#include "stm32f4xx.h"
+#include "./led/bsp_led.h"
+#include "./usart/bsp_debug_usart.h"
+#include "./adc/bsp_adc.h"
+#include "./beep/bsp_beep.h"
+
+
+// ADC1转换的电压值通过MDA方式传到SRAM
+extern __IO uint32_t ADC_ConvertedValue[3];
+
+// 局部变量，用于保存转换计算后的电压值 	 
+float ADC_ConvertedValueLocal[3]; 
+
+static void Delay(__IO uint32_t nCount)	 //简单的延时函数
+{
+	for(; nCount != 0; nCount--);
+}
+
+typedef enum {
+    SYSTEM_NORMAL = 0,     // 系统正常
+    SYSTEM_VOLTAGE_LOW,    // 电压过低
+    SYSTEM_VOLTAGE_HIGH,   // 电压过高
+    SYSTEM_ERROR           // 程序异常
+} System_Status;
+
+static System_Status system_status = SYSTEM_NORMAL;
+
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{	
+  /*初始化USART 配置模式为 115200 8-N-1，中断接收*/
+  Debug_USART_Config();
+	LED_GPIO_Config();
+	BEEP_GPIO_Config();
+	Rheostat_Init();	
+	
+	printf("系统启动完成...\r\n");
+  printf("电压正常范围: %.1fV - %.1fV\r\n", VOLTAGE_MIN, VOLTAGE_MAX);
+	
+  while (1)
+  {
+    Delay(0xffffee);  
+    
+    ADC_ConvertedValueLocal[0] =(float)((uint16_t)ADC_ConvertedValue[0]*3.3/4096); 
+    ADC_ConvertedValueLocal[1] =(float)((uint16_t)ADC_ConvertedValue[1]*3.3/4096);
+    ADC_ConvertedValueLocal[2] =(float)((uint16_t)ADC_ConvertedValue[2]*3.3/4096);
+    
+		// 设置LED状态
+    LED_SetStatus(system_status);
+		
+    printf("\r\n The current AD value = 0x%08X \r\n", ADC_ConvertedValue[0]); 
+    printf("\r\n The current AD value = 0x%08X \r\n", ADC_ConvertedValue[1]); 
+    printf("\r\n The current AD value = 0x%08X \r\n", ADC_ConvertedValue[2]); 
+    
+    printf("\r\n The current ADC1 value = %f V \r\n",ADC_ConvertedValueLocal[0]); 
+    printf("\r\n The current ADC2 value = %f V \r\n",ADC_ConvertedValueLocal[1]);
+    printf("\r\n The current ADC3 value = %f V \r\n",ADC_ConvertedValueLocal[2]);
+  
+	  // 打印系统状态
+        switch(system_status) {
+            case SYSTEM_NORMAL:
+                printf("系统状态: 正常\r\n");
+                break;
+            case SYSTEM_VOLTAGE_LOW:
+                printf("系统状态: 电压过低!\r\n");
+                break;
+            case SYSTEM_VOLTAGE_HIGH:
+                printf("系统状态: 电压过高!\r\n");
+                break;
+            case SYSTEM_ERROR:
+                printf("系统状态: 程序异常!\r\n");
+                break;
+					}
+				printf("==================\r\n");
+	}
+}
---
+++ User/led/bsp_led.h
@@ -0,0 +1,47 @@
+#ifndef __LED_H
+#define	__LED_H
+
+#include "stm32f4xx.h"
+
+//引脚定义
+/*******************************************************/
+//R 红色灯
+#define LED1_PIN                  GPIO_Pin_6                 
+#define LED1_GPIO_PORT            GPIOF                      
+#define LED1_GPIO_CLK             RCC_AHB1Periph_GPIOF
+
+//G 绿色灯
+#define LED2_PIN                  GPIO_Pin_7                 
+#define LED2_GPIO_PORT            GPIOF                      
+#define LED2_GPIO_CLK             RCC_AHB1Periph_GPIOF
+
+#define VOLTAGE_MIN    1.0f    // 最低正常电压
+#define VOLTAGE_MAX    2.5f    // 最高正常电压
+
+/************************************************************/
+
+
+/** 控制LED灯亮灭的宏，
+	* LED低电平亮，设置ON=0，OFF=1
+	* 若LED高电平亮，把宏设置成ON=1 ，OFF=0 即可
+	*/
+#define ON  0
+#define OFF 1
+
+/* 带参宏，可以像内联函数一样使用 */
+#define LED1(a)	if (a)	\
+					GPIO_SetBits(LED1_GPIO_PORT,LED1_PIN);\
+					else		\
+					GPIO_ResetBits(LED1_GPIO_PORT,LED1_PIN)
+
+#define LED2(a)	if (a)	\
+					GPIO_SetBits(LED2_GPIO_PORT,LED2_PIN);\
+					else		\
+					GPIO_ResetBits(LED2_GPIO_PORT,LED2_PIN)
+
+
+
+void LED_GPIO_Config(void);
+void LED_SetStatus();
+
+#endif /* __LED_H */
