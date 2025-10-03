ef20383d501d5b538f973613835a1951e7388687
Kakuro8217 <Kakuro8217@163.com>
2025-10-03 09:57:22 +0800

    Update bsp_beep.h

--- User/beep/bsp_beep.h
+++ User/beep/bsp_beep.h
@@ -5,16 +5,16 @@
 #include "stm32f4xx.h"
 
 
-/* ������������ӵ�GPIO�˿�, �û�ֻ��Ҫ�޸�����Ĵ��뼴�ɸı���Ƶķ��������� */
-#define BEEP_GPIO_PORT    	GPIOG			              /* GPIO�˿� */
-#define BEEP_GPIO_CLK 	    RCC_AHB1Periph_GPIOG		/* GPIO�˿�ʱ�� */
-#define BEEP_GPIO_PIN		  	GPIO_Pin_7			        /* ���ӵ���������GPIO */
+/* 定义蜂鸣器连接的GPIO端口, 用户只需要修改下面的代码即可改变控制的蜂鸣器引脚 */
+#define BEEP_GPIO_PORT    	GPIOG			              /* GPIO端口 */
+#define BEEP_GPIO_CLK 	    RCC_AHB1Periph_GPIOG		/* GPIO端口时钟 */
+#define BEEP_GPIO_PIN		  	GPIO_Pin_7			        /* 连接到蜂鸣器的GPIO */
 
-/* �ߵ�ƽʱ���������� */
-#define ON  1
-#define OFF 0
+/* 高电平时，蜂鸣器响 */
+#define BEEP_ON  1
+#define BEEP_OFF 0
 
-/* ���κ꣬��������������һ��ʹ�� */
+/* 带参宏，可以像内联函数一样使用 */
 #define BEEP(a)	if (a)	\
 					GPIO_SetBits(BEEP_GPIO_PORT,BEEP_GPIO_PIN);\
 					else		\
